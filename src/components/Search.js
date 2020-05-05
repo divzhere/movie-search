0f624e46e40aba8e19e8c08f9ed643f70e7a4537
Divyum <divz7777@gmail.com>
2020-05-06 02:58:27 +0530

    search styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,10 +4,22 @@ export default class Search extends Component {
     render() {
         return (
             <div>
-                <form class="example" style={{margin:'auto',maxWidth:'300px'}}>
+                {/* <form class="example" style={{margin:'auto',maxWidth:'300px'}}>
                 <input type="text" placeholder="Search Movie Title"></input>
                 <button type="submit"><i className="material-icons">search</i></button>
                 </form>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <hr></hr> */}
+                <div className="box">
+                    <form>
+                        <input type="text" name="" placeholder="Search Movie Title"></input>
+                        <button type="submit"><span className="material-icons">search</span></button>
+                    </form>
+                </div>
             </div>
         )
     }
