df874021064adb0c253a707808d91e2fc6334e89
Divyum <divz7777@gmail.com>
2020-05-06 21:41:13 +0530

    redux devtools installed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux'
 import App from './App';
 import configureStore from './store'
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore()}>
